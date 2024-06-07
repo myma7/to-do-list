1f6ac4c044dd47e98f22391099af3934f15ec8bb
Joanna Buczylo <a.buczylo@gmail.com>
2024-06-07 19:55:02 +0200

    Adding styles using Sass

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function addRandomElements() {
     for (let i = 0; i < elements.length; i++) {
         let createElementLiForList = document.createElement("li");
         createElementLiForList.className = "single-element";
-        createElementLiForList.innerHTML = ` ${elements[i]} <button class="btn-click-s" onclick="removeElement(this)">-</button> <button  class="btn-click-s" onclick="doneElement(this)">+</button>`;
+        createElementLiForList.innerHTML = ` ${elements[i]} <button class="btn-click-simple" onclick="removeElement(this)">❌</button> <button  class="btn-click-simple" onclick="doneElement(this)">✅</button>`;
         listElements.appendChild(createElementLiForList);
     }
 }
@@ -23,12 +23,11 @@ function addItemToList() {
 
         let createElementLiForList = document.createElement("li");
         createElementLiForList.className = "single-element";
-        createElementLiForList.innerHTML = `${newItem} <button  class="btn-click-s" onclick="removeElement(this)">-</button> <button  class="btn-click-s" onclick="doneElement(this)">+</button>`;
+        createElementLiForList.innerHTML = `${newItem} <button  class="btn-click-simple" onclick="removeElement(this)">❌</button> <button  class="btn-click-simple" onclick="doneElement(this)">✅</button>`;
         listElements.appendChild(createElementLiForList);
 
         searchbar.value = "";
     }
-    console.log(listForAllElementsInLists);
 }
 
 function doneElement(button) {
@@ -55,30 +54,4 @@ function searchForItem() {
     }
 }
 
-// Initialize the list with random elements on page load
 addRandomElements();
-
-
-
-
-// function search() {
-//     let filter = document.getElementById("searchbar").value.toUpperCase();
-//     let ul = document.getElementById("list");
-//     let li = ul.getElementsByTagName("li");
-
-//     for (let i = 0; i < li.length; i++) {
-//         let txtValue = li[i].textContent || li[i].innerText;
-//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//             li[i].style.display = "";
-//         } else {
-//             li[i].style.display = "none";
-//         }
-//     }
-
-// function getDate1() {
-//     var date = new Date();
-//     var day = date.getDate(); // Corrected method call
-//     var hour = date.getHours(); // Corrected method call
-//     var minutes = date.getMinutes(); // Corrected method call
-//     info.innerHTML = `Date: ${day}, Time: ${hour}:${minutes < 10 ? '0' : ''}${minutes}`; // Improved formatting
-// }
